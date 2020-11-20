5af4a3c72cd269b4425f3bc536c5c81f7195ad3a
jksrijay <srijayjk@gmail.com>
2020-11-20 22:50:33 +0100

    Train Test Split and Reshape Input to 3D for GRU

--- SEMG_5010.py
+++ SEMG_5010.py
@@ -4,6 +4,8 @@ import pandas as pd
 import scipy.io
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow import keras
 
 # Read CSV using Pandas Framework and covert data to pandas DataFrame
 data = pd.read_csv(r"C:\Users\SRIJAY\Desktop\Research Project\Data SET\50mus-10s\50mus-10s\electrodes.csv",
@@ -17,8 +19,10 @@ time_data = data['t']
 electrode_data = data.drop(data.iloc[:, :1155], axis=1)
 position_data = data.drop(data.iloc[:, 1155:1539], axis=1)
 position_data = position_data.drop(position_data.iloc[:, 0:3], axis=1)
-print(time_data)
-print(electrode_data)
+
+
+# print(time_data)
+# print(electrode_data)
 
 
 # Function for Normalizing the Data.
@@ -43,8 +47,8 @@ for line in lines:
     lines = [float(i) for i in lines]
     log.append(lines)
 lab = pd.DataFrame(log)
-lab = lab.drop_duplicates(0).drop(lab.columns[1], axis=1).replace(np.nan, 0)
 lab = lab.sort_values(by=0)
+lab = lab.drop_duplicates(0).drop(lab.columns[0:1], axis=1).replace(np.nan, 0)
 print(lab.duplicated().any())
 
 
@@ -56,28 +60,38 @@ def round_labels(number):
 rounded_labels = round_labels(lab)
 
 # This part converts log file to labels.
-lab1 = rounded_labels.iloc[:, 1:]
-f = []
-for i in range(len(lab1)):
-    for j in range(len(lab1.iloc[i, :])):
-        if (lab1.iloc[i, j] != 0).all():
-            f.append(i + 1)
-        else:
-            f.append(0)
-f[0] = 1
-print(f)
+lab1 = rounded_labels.iloc[:, 1:]  # labels are rounded
+# f = []
+# for i in range(len(lab1)):
+#     for j in range(len(lab1.iloc[i, :])):
+#         if (lab1.iloc[i, j] != 0).all():
+#             f.append(i + 1)
+#         else:
+#             f.append(0)
+# f[0] = 1
+# print(f)
 
 labels = np.zeros(20000)
-for i in range(len(lab)):
-    for j in range(len(lab.iloc[i, :])):
-        if lab.iloc[i, j] != 0:
-            labels[int((lab.iloc[i, j]) * 2)] = i + 1
-
-print(labels)
+for i in range(len(lab1)):
+    for j in range(len(lab1.iloc[i, :])):
+        if lab1.iloc[i, j] != 0:
+            labels[int((lab1.iloc[i, j]) * 2)] = i + 1
+        else:
+            labels[int((lab1.iloc[i, j]) * 2)] = 0
+labels[0] = 1
 
 # Train Test Split
 
 X_train, X_test, y_train, y_test = train_test_split(Norm_electrode_data, labels, test_size=0.33)
-print(X_train.shape)
-print(y_train.shape)
+print('X_train:', X_train.shape, 'y_train:', y_train.shape)
+print('X_test:', X_test.shape, 'y_test:', y_test.shape)
 
+plt.hist(labels, bins=50)
+plt.show()
+
+# Reshape input to be 3D [samples, timesteps, features] as expected by GRU
+
+X_train = X_train.reshape((X_train.shape[0], 1, X_train.shape[1]))
+X_test = X_test.reshape((X_test.shape[0], 1, X_test.shape[1]))
+
+print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
