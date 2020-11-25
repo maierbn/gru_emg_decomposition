e00547db2f6de53706e8ce18e8ee9f0f8a013ffc
jksrijay <srijayjk@gmail.com>
2020-11-25 19:58:32 +0100

    Changed the Input Shape, Created GRU Model

--- SEMG_5010.py
+++ SEMG_5010.py
@@ -1,3 +1,4 @@
+import keras as keras
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -6,8 +7,11 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow import keras
-
+from keras.utils import to_categorical
+from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
 # Read CSV using Pandas Framework and covert data to pandas DataFrame
+from tensorflow.python.keras.optimizer_v2.adam import Adam
+
 data = pd.read_csv(r"C:\Users\SRIJAY\Desktop\Research Project\Data SET\50mus-10s\50mus-10s\electrodes.csv",
                    delimiter=';', header=0, na_values=['NA'], encoding='ISO-8859-9')
 data = pd.DataFrame(data)
@@ -61,15 +65,6 @@ rounded_labels = round_labels(lab)
 
 # This part converts log file to labels.
 lab1 = rounded_labels.iloc[:, 1:]  # labels are rounded
-# f = []
-# for i in range(len(lab1)):
-#     for j in range(len(lab1.iloc[i, :])):
-#         if (lab1.iloc[i, j] != 0).all():
-#             f.append(i + 1)
-#         else:
-#             f.append(0)
-# f[0] = 1
-# print(f)
 
 labels = np.zeros(20000)
 for i in range(len(lab1)):
@@ -86,12 +81,64 @@ X_train, X_test, y_train, y_test = train_test_split(Norm_electrode_data, labels,
 print('X_train:', X_train.shape, 'y_train:', y_train.shape)
 print('X_test:', X_test.shape, 'y_test:', y_test.shape)
 
-plt.hist(labels, bins=50)
-plt.show()
+#plt.hist(labels, bins=51)
+#plt.show()
+
+#  One hot coding using Keras Categorical
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+print(y_train.shape)
 
 # Reshape input to be 3D [samples, timesteps, features] as expected by GRU
+look_back = 80
+batch_size = 512
+train_data_gen = TimeseriesGenerator(X_train, y_train, length=look_back, batch_size=batch_size, shuffle=True)
+test_data_gen = TimeseriesGenerator(X_test, y_test, length=look_back, batch_size=batch_size)
 
-X_train = X_train.reshape((X_train.shape[0], 1, X_train.shape[1]))
-X_test = X_test.reshape((X_test.shape[0], 1, X_test.shape[1]))
+# Define Hyperparameters.
+epochs = 400
+learning_rate = 0.001
 
-print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+# Creating GRU model using tensorflow Keras
+
+model = keras.Sequential([
+    keras.layers.Bidirectional(keras.layers.GRU(units=80, return_sequences=True, activation='tanh'), input_shape=(80, 384)),
+    keras.layers.Dropout(0.5),
+    keras.layers.Bidirectional(keras.layers.GRU(units=80, activation='tanh')),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(units=51, activation='softmax')
+])
+
+model.summary()
+# callback = keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+opt = keras.optimizers.Adam(learning_rate=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+model.compile(optimizer=opt, loss='mean_squared_error', metrics=['accuracy'])
+history = model.fit(train_data_gen, validation_data=test_data_gen, steps_per_epoch=26, epochs=1)  # add callback for early stopping
+
+
+# evaluate the model
+_, train_acc = model.evaluate(train_data_gen, verbose=0)
+_, test_acc = model.evaluate(test_data_gen, verbose=0)
+print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))
+
+
+# # plot loss during training
+# plt.subplot(211)
+# plt.title('Loss')
+# plt.plot(history.history['loss'], label='train')
+# plt.plot(history.history['val_loss'], label='test')
+# plt.legend()
+# # plot accuracy during training
+# plt.subplot(212)
+# plt.title('Accuracy')
+# plt.plot(history.history['accuracy'], label='train')
+# plt.plot(history.history['val_accuracy'], label='test')
+# plt.legend()
+# plt.show()
+
+
+# preds_classes = model.predict_classes(train_data_gen)
+# for i in range(len(train_data_gen)):
+#     print("Predicted=%s" % (preds_classes[i]))
